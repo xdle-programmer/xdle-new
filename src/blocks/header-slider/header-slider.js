92266a358184c874f77a81ec08d3807efd5f8d3b
Sergey <act1987@mail.ru>
2021-02-11 23:19:24 +0200

    Animation

---
+++ src/blocks/header-slider/header-slider.js
@@ -0,0 +1,56 @@
+let $sliderMain = $('.header-slider'),
+    $sliderWrapper = $('.header-slider__wrapper'),
+    $sliderContainer = $('.header-slider__container'),
+    $sliderItems = $(' .header-slider__item'),
+    heightPage = $(window).height(),
+    resultHeight = 0,
+    resultWidth = 0,
+    countSection = $sliderItems.length;
+
+resultHeight = countSection * heightPage;
+$sliderMain.height(resultHeight)
+ console.log(heightPage)
+ $(window).on('scroll', function () {
+     let scrollOffset = $(window).scrollTop()
+
+     // console.log(scrollOffset)
+     // console.log(resultHeight)
+
+     if(scrollOffset > 0) {
+         $sliderWrapper.css('transform', 'translate(' + -50 + '%, -' + scrollOffset + 'px)');
+     }
+
+ });
+
+$('.header-slider__navigation-item').on('click', function(){
+    // console.log($(this).index());
+    $('.header-slider__navigation-item').removeClass('header-slider__navigation-item--active');
+    $(this).addClass('header-slider__navigation-item--active');
+
+    $('.header-slider__item').toggleClass('header-slider__item--active');
+})
+
+// let $slideImages = $('.header-slider__image-wrapper .header-slider__image');
+// let numberRandom = 1 + Math.floor(Math.random() * 6);
+// console.log(numberRandom)
+// $slideImages.each(function() {
+//     let currentImage = $(this);
+//     $('body').on('mousemove', function(e){
+//
+//         let x = e.pageX - this.offsetLeft;
+//         let y = e.pageY - this.offsetTop;
+//
+//         if(!currentImage.hasClass('header-slider__image-el-1')) {
+//             currentImage.css({
+//                 'transform': 'translate(' + x/1000 + '%, -' + y/100 + 'px)'
+//             })
+//         }
+//
+//     });
+// });
+
+
+
+
+
+
